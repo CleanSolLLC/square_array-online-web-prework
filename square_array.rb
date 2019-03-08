071ab8c7fa69d9df9aa63ba9b3aee930a5500599
Mark May <mamark90@gmail.com>
2019-03-08 14:39:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 array = [1,2,3]
 
-def num_array(array)
-num_squared = []
-array.each{|x| num_squared << x**2}
-num_squared
+def square_array(array)
+  num_squared = []
+  array.each{|x| num_squared << x**2}
+  num_squared
 end
